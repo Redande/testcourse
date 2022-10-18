773cb21f879643f8e159a96c5c6382bbeb0638c9
Antti Leinonen <antti.leinonen@helsinki.fi>
2022-10-18 16:21:44 +0300

    Add an exercise with no points

---
+++ no_points/test/TrivialTest.java
@@ -0,0 +1,12 @@
+
+import fi.helsinki.cs.tmc.edutestutils.Points;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TrivialTest {
+    @Test
+    public void testF() {
+        Trivial t = new Trivial();
+        assertEquals(1, t.f());
+    }
+}
